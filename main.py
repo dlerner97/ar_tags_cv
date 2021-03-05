9741c2b75a37859abe0d511c09d3279b28610acb
Dani Lerner <dlerner97@gmail.com>
2021-03-05 14:09:13 -0500

    Added QR reader

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import os
 import cv2
 import math
 import numpy as np
-from numpy.core.arrayprint import set_printoptions
 import pandas as pd
 from scipy import fftpack
 from matplotlib import pyplot as plt
+
 
 class CV:
     def __init__(self, vid_path):
@@ -19,7 +19,7 @@ class CV:
         dim = (width, height)
         return cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
-    def separate_background(self, frame, filter_size=100):
+    def separate_background_w_fft(self, frame, filter_size=100):
         def disp_fft(img):
             plt.figure()
             plt.imshow(np.log(1+np.abs(img)).real, "gray") 
@@ -40,139 +40,32 @@ class CV:
         max_val = np.max(ifft)
         gray2uint8 = lambda px: np.uint8(255*(px - min_val)/(max_val-min_val))
         mat = np.asarray(list(map(gray2uint8, ifft)))
+        _,mat = cv2.threshold(mat, 150, 255, cv2.THRESH_BINARY)
+        mat = cv2.dilate(mat, (57,57))
+        # mat = cv2.morphologyEx(mat, cv2.MORPH_OPEN, np.ones((3,3), np.uint8))
+        # mat = cv2.morphologyEx(mat, cv2.MORPH_CLOSE, np.ones((31,31), np.uint8))
         return mat
 
 if __name__ == '__main__':
-    # os.system('cls')
-    os.system('clear')
+    os.system('cls')
+    # os.system('clear')
     K = np.array([[1406.08415449821,                0, 0],
                   [2.20679787308599, 1417.99930662800, 0],
                   [1014.13643417416, 566.347754321696, 1]])
 
-    cv = CV('Tag1.mp4')
-    counter = 0
-    while True:
-        counter += 1
-        ret, frame = cv.video_feed.read()
+    cv = CV('Tag0.mp4')
+    _, frame = cv.video_feed.read()
 
-        if not ret:
-            break
+    tag = cv.separate_background_w_fft(frame, filter_size=30)
+    non0 = np.nonzero(tag)
 
-        # tag = cv.separate_background(frame, filter_size=220)
-        # tag = CV.resize_frame(tag, 50)
-        # cv2.imshow("tag", tag)
-        # cv2.waitKey(0)
+    detected = frame.copy()
+    detected[non0[0], non0[1]] = (0,0,255)
+    img = np.vstack((frame, detected))
 
-        # Convert to grayscale
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # tag[non0[0], non0[1]] = 0
+    img = CV.resize_frame(img, 20)
+    cv2.imshow("all", img)
+    cv2.waitKey(0)
 
-        # Apply Gaussian Blur to the image (11x11)
-        blur = cv2.GaussianBlur(frame, (11,11), 0)
 
-        # Apply Canny edge detector to the image
-        edges = cv2.Canny(blur, 200, 250)
-
-        # Apply a closing morphilogical transformation to the image. This will act as a mask
-        closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, np.ones((61,61), np.uint8))
-        
-        non0px = np.nonzero(closing)
-        med_row = int(np.median(non0px[0]))
-        med_col = int(np.median(non0px[1]))
-
-        roi_buffer = 150
-        closing[:(med_row-roi_buffer),:] = 0
-        closing[(med_row+roi_buffer):, :] = 0
-
-        closing[:, :(med_col-roi_buffer)] = 0
-        closing[:, (med_col+roi_buffer):] = 0
-
-        # 
-        masked = closing * frame
-        masked = cv2.morphologyEx(masked, cv2.MORPH_OPEN, np.ones((5,5), np.uint8))
-        corners = cv2.cornerHarris(masked, 10, 11, .05)
-        # frame_cp = masked.copy()
-        frame_cp = np.zeros_like(frame)
-        
-        corners = cv2.dilate(corners, None)
-        frame_cp[corners>.01*corners.max()] = 255
-
-        non0px = np.nonzero(frame_cp)
-        med_x = np.median(non0px[0])
-        med_y = np.median(non0px[1])
-
-        try:
-            med_x = int(med_x)
-            med_y = int(med_y)
-        except ValueError:
-            continue
-
-        # roi = np.where(frame)
-
-        min_x_arg = np.min(non0px[0])
-        max_x_arg = np.max(non0px[0])
-        min_y_arg = np.min(non0px[1])
-        max_y_arg = np.max(non0px[1])
-
-        min_dist = min((max_x_arg-min_x_arg, max_y_arg-min_y_arg))
-        print(f"frame {counter}: {min_dist}")
-
-        dilate_size = 1
-        if min_dist > 250:
-            dilate_size = 15
-        elif min_dist > 230:
-            dilate_size = 13
-        elif min_dist > 200:
-            dilate_size = 11
-        elif min_dist > 170:
-            dilate_size = 9
-        elif min_dist > 130:
-            dilate_size = 7
-        elif min_dist > 100:
-            dilate_size = 5
-        elif min_dist > 75:
-            dilate_size = 3
-
-        frame_cp = cv2.dilate(frame_cp, np.ones((dilate_size, dilate_size), dtype=np.uint8))
-
-        # ret, dst = cv.threshold(dst,0.01*dst.max(),255,0)
-        # dst = np.uint8(dst)
-        # # find centroids
-        ret, labels, stats, centroids = cv2.connectedComponentsWithStats(frame_cp)
-        # print(f"ret {ret}", f"labels {labels}", f"stats {stats}", f"centroids{centroids}", sep='\n')
-        
-        corner_filter_1 = None
-        num_arr = 5
-        while True:
-            try:
-                corner_filter_1 = np.argpartition(stats[:,-1], num_arr)[:num_arr]
-                break
-            except:
-                num_arr -= 1
-
-        filtered_1 = centroids[corner_filter_1]
-
-        min_x_arg = np.argmin(filtered_1[:,0])
-        max_x_arg = np.argmax(filtered_1[:,0])
-        min_y_arg = np.argmin(filtered_1[:,1])
-        max_y_arg = np.argmax(filtered_1[:,1])
-
-        wait = 0
-        try:
-            filtered_2 = filtered_1[np.sum(list(range(5)))-(min_x_arg+min_y_arg+max_x_arg+max_y_arg)]
-            filtered_2 = (int(round(filtered_2[0])), int(round(filtered_2[1])))
-            # print(filtered_2)
-            cv2.circle(frame, filtered_2, 5, 0, -1)
-        except IndexError:
-            print("Index Error")
-            # wait = 0
-
-        cv2.circle(frame, (med_y, med_x), 5, 0, -1)
-        img_top = np.hstack((frame, closing))
-        img_bot = np.hstack((masked, frame_cp))
-        img = np.vstack((img_top, img_bot))
-        img = CV.resize_frame(img, 30)
-
-        cv2.imshow("img", img)
-        k = cv2.waitKey(wait) & 0xff
-        if k == ord('q'):
-            break
---
+++ read_ar.py
@@ -0,0 +1,81 @@
+import os
+import cv2
+import numpy as np
+
+class ARReader:
+    def __init__(self) -> None:
+        self.uint8_0 = np.uint8(0)
+        self.uint8_half = np.uint8(255//2)
+    
+    def sim_cv_output(self, tag):
+        
+        return tag      
+
+    def read_tag(self, tag, ret_img=False):
+        non0pts = np.nonzero(tag)
+
+        min_non_zero_row = min(non0pts[0])
+        max_non_zero_row = max(non0pts[0])
+        min_non_zero_col = min(non0pts[1])
+        max_non_zero_col = max(non0pts[1])
+        
+        span_rows = max_non_zero_row - min_non_zero_row
+        span_cols = max_non_zero_col - min_non_zero_col
+        
+        num_bins = 8
+        bin_rows = int(round(span_rows/num_bins))
+        bin_cols = int(round(span_cols/num_bins))
+        
+        full_ar_map = np.zeros((num_bins, num_bins), np.uint8)
+        
+        if not ret_img:
+            for i in range(num_bins+1):
+                cv2.line(tag, (min_non_zero_col, i*bin_rows), (max_non_zero_col, i*bin_rows), 150, 2)
+                cv2.line(tag, (i*bin_cols, min_non_zero_row), (i*bin_cols, max_non_zero_row), 150, 2)
+        
+        for row in range(num_bins):
+            for col in range(num_bins):
+                temp_section = tag[row*bin_rows:(row+1)*bin_rows, col*bin_cols: (col+1)*bin_cols]
+                full_ar_map[row, col] = np.median(temp_section)
+            
+        lower = num_bins//4
+        upper = lower*3
+        qr_tag = full_ar_map[lower:upper, lower:upper]
+                        
+        get_binary_str = lambda coord: '1' if qr_tag[coord] < self.uint8_half else '0'
+        bit_11 = get_binary_str((1,1))
+        bit_12 = get_binary_str((1,2))
+        bit_21 = get_binary_str((2,1))
+        bit_22 = get_binary_str((2,2))
+        
+        binary_word = {'00'  : int(bit_12+bit_11+bit_21+bit_22,2),
+                       '0n1' : int(bit_22+bit_12+bit_11+bit_21,2),
+                       'n10' : int(bit_11+bit_21+bit_22+bit_12,2),
+                       'n1n1': int(bit_21+bit_22+bit_12+bit_11,2)}
+                
+        word = None
+        if qr_tag[0,0] < self.uint8_half:
+            word = binary_word["00"]
+        elif qr_tag[0,-1] < self.uint8_half:
+             word = binary_word["0n1"]
+        elif qr_tag[-1,0] < self.uint8_half:
+             word = binary_word["n10"]
+        elif qr_tag[-1,-1] <  self.uint8_half:
+             word = binary_word["n1n1"]
+        else:
+            print("Cannot find proper orientation.")     
+            
+        return word, tag
+        
+if __name__ == '__main__':
+    os.system('cls')
+    img = cv2.imread("clean_ar_tag.png")
+    # img = cv2.imread("ref_marker2.png")
+    # img = cv2.imread("ref_marker3.png")
+    # img = cv2.imread("ref_marker4.png")
+    reader = ARReader()
+    img = 255 - img
+    word, tag = reader.read_tag(img)
+    print(f"ID: {word}")
+    cv2.imshow("Tag w/ grid", tag)
+    cv2.waitKey(0)
